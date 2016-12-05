59bcd7f802c29374d6ef625eab31c3fed1545712
kwrl <kwrl@kwrls-MacBook-Pro.local>
2016-12-05 22:53:07 +0100

    Useless attempts at performance improvements

--- adventofcode/src/adventofcode/task5/Task5.java
+++ adventofcode/src/adventofcode/task5/Task5.java
@@ -4,48 +4,27 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Task5 {
-	
-	private static byte[] hash(String doorID, int salt) throws NoSuchAlgorithmException {
-		MessageDigest md5 = MessageDigest.getInstance("MD5");
-		return md5.digest((doorID + salt).getBytes());
-	}
-
-	public static String toHexString(byte[] bytes) {
-		StringBuilder hexString = new StringBuilder();
-
-		for (int i = 0; i < bytes.length; i++) {
-			String hex = Integer.toHexString(0xFF & bytes[i]);
-			if (hex.length() == 1) {
-				hexString.append('0');
-			}
-			hexString.append(hex);
-		}
-
-		return hexString.toString();
-	}
-
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		long start = System.currentTimeMillis();
+
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
 		String input = "abbhdwsy";
 
 		int count = 0;
 		char[] output = new char[8];
 		int idx = 0;
-		
+
 		while (count < 8) {
-			byte[] hashBytes = hash(input, idx++);
+			byte[] hashBytes = md5.digest((input + idx++).getBytes());
 
 			if ((hashBytes[0] | hashBytes[1]) != 0 || (0xFF & hashBytes[2]) > 0x0F) {
 				continue;
 			}
-
-			String hash = toHexString(hashBytes);
-
-			int pos = hash.charAt(5) - 48;
+			int pos = hashBytes[2] & 0xFF;
 			if (pos < 0 || pos >= output.length || output[pos] != 0) {
 				continue;
 			}
-			output[pos] = hash.charAt(6);
+			output[pos] = Integer.toHexString((hashBytes[3] & 0xF0) >> 4).charAt(0);
 			count++;
 		}
 		System.out.println((System.currentTimeMillis() - start) / 1000);
