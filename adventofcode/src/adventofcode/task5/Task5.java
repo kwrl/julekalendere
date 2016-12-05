4d6af27dc4b63f4cabea8c03cd095e0a15614b23
kwrl <kwrl@dhcp-10-22-42-152.wlan.ntnu.no>
2016-12-05 21:15:44 +0100

    Performance improvements

--- adventofcode/src/adventofcode/task5/Task5.java
+++ adventofcode/src/adventofcode/task5/Task5.java
@@ -4,45 +4,51 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Task5 {
-	private static String hash(String doorID, int salt) throws NoSuchAlgorithmException {
+	
+	private static byte[] hash(String doorID, int salt) throws NoSuchAlgorithmException {
 		MessageDigest md5 = MessageDigest.getInstance("MD5");
-		return toHexString(md5.digest((doorID+salt).getBytes()));
+		return md5.digest((doorID + salt).getBytes());
 	}
-	
+
 	public static String toHexString(byte[] bytes) {
-	    StringBuilder hexString = new StringBuilder();
+		StringBuilder hexString = new StringBuilder();
 
-	    for (int i = 0; i < bytes.length; i++) {
-	        String hex = Integer.toHexString(0xFF & bytes[i]);
-	        if (hex.length() == 1) {
-	            hexString.append('0');
-	        }
-	        hexString.append(hex);
-	    }
+		for (int i = 0; i < bytes.length; i++) {
+			String hex = Integer.toHexString(0xFF & bytes[i]);
+			if (hex.length() == 1) {
+				hexString.append('0');
+			}
+			hexString.append(hex);
+		}
 
-	    return hexString.toString();
-	}	
-	
-	
+		return hexString.toString();
+	}
+
 	public static void main(String[] args) throws NoSuchAlgorithmException {
+		long start = System.currentTimeMillis();
 		String input = "abbhdwsy";
-		
+
 		int count = 0;
 		char[] output = new char[8];
 		int idx = 0;
 		
-		while(count < 8) {
-			String hash = hash(input, idx++);
-			
-			if(hash.startsWith("00000")) {
-				int pos = hash.charAt(5) - 48;
-				if(pos < 0 || pos >= output.length || output[pos] != 0) {
-					continue;
-				}
-				output[pos] = hash.charAt(6);
-				count++;
+		while (count < 8) {
+			byte[] hashBytes = hash(input, idx++);
+
+			if ((hashBytes[0] | hashBytes[1]) != 0 || (0xFF & hashBytes[2]) > 0x0F) {
+				continue;
 			}
+
+			String hash = toHexString(hashBytes);
+
+			int pos = hash.charAt(5) - 48;
+			if (pos < 0 || pos >= output.length || output[pos] != 0) {
+				continue;
+			}
+			output[pos] = hash.charAt(6);
+			count++;
 		}
+		System.out.println((System.currentTimeMillis() - start) / 1000);
 		System.out.println(new String(output));
 	}
 }
